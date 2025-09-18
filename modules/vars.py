2f7ea98a35b0cea3de4930e77fdc2ca5552b0e69
hercules74647329-sudo <h74647329@gmail.com>
2025-09-18 17:20:16 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "21611775"))
 API_HASH = environ.get("API_HASH", "26ae420a7d0d7f1419f894e03b35cc4e")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8473074783:AAGO2rt_rgMZlM-8CEckspFd_B4StVgLF6c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8357080627:AAEAiqWY9VNCXiEi_oQDwu5djdNI2WLP8dM")
 
 OWNER = int(environ.get("OWNER", "1110898121"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
@@ -23,6 +23,7 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
